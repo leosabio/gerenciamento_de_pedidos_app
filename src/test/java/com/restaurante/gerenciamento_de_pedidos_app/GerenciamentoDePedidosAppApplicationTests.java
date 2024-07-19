ef25ad7ea1baba17ddabb1d58ac860c38b09a7fc
leosabio <leosabiot@gmail.com>
2024-07-19 02:01:19 +0100

    Configuração base de dados

---
+++ src/test/java/com/restaurante/gerenciamento_de_pedidos_app/GerenciamentoDePedidosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurante.gerenciamento_de_pedidos_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDePedidosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
